fixed bug in the calculation of state values; swapped initial state value for sampled return

envs.py
@@ -5,13 +5,16 @@ import numpy as np
 
 class GridWorld:
 
-    def __init__(self, gamma, initial_state=(1, 1), terminal_state=(9, 9)):
+    def __init__(self, gamma=0.9, initial_state=(1, 1), terminal_state=(9, 9)):
+        assert(0 <= gamma < 1)
         assert((initial_state[0] != terminal_state[0]) or (initial_state[1] != terminal_state[1]))
         self.gamma = gamma
-        self.card = np.array([11, 11])
-        self.num_actions = 4
         self.initial_state = np.array(initial_state, dtype=np.uint8)
         self.terminal_state = np.array(terminal_state, dtype=np.uint8)
+        self.card = np.array([11, 11])
+        self.min_value = 0
+        self.max_value = 1
+        self.num_actions = 4
         self.walls = np.array(
             [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
              [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
@@ -24,91 +27,116 @@ class GridWorld:
              [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
              [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]], dtype=bool)
+        assert(self._is_valid(self.initial_state))
+        assert(self._is_valid(self.terminal_state))
         self.reset()
+
+    def _is_wall(self, state):
+        return self.walls[state[0], state[1]]
+
+    def _is_valid(self, state):
+        if len(state) != 2:
+            return False
+        if not ((0 < state[0] < 10) and (0 < state[1] < 10)):
+            return False
+        if self._is_wall(state):
+            return False
+        return True
+
+    def _is_terminal(self, state):
+        return (state[0] == self.terminal_state[0]) and \
+               (state[1] == self.terminal_state[1])
+
+    def set_state(self, state):
+        assert(self._is_valid(state))
+        self.state = np.copy(state)
 
     def get_state(self):
         return np.copy(self.state)
-
-    def set_state(self, state):
-        self.state = np.copy(state)
 
     def get_next_state(self, action, state=None):
         if state is None:
             state = self.get_state()
         else:
             state = np.copy(state)
-        if action == 0:
-            if not self.is_wall((state[0], state[1] - 1)):
-                state[1] -= 1
-        elif action == 1:
-            if not self.is_wall((state[0] + 1, state[1])):
-                state[0] += 1
-        elif action == 2:
-            if not self.is_wall((state[0], state[1] + 1)):
-                state[1] += 1
-        else:
-            assert(action == 3)
-            if not self.is_wall((state[0] - 1, state[1])):
-                state[0] -= 1
+            assert(self._is_valid(state))
+        if not self._is_terminal(state):
+            if action == 0:
+                if not self._is_wall((state[0], state[1] - 1)):
+                    state[1] -= 1
+            elif action == 1:
+                if not self._is_wall((state[0] + 1, state[1])):
+                    state[0] += 1
+            elif action == 2:
+                if not self._is_wall((state[0], state[1] + 1)):
+                    state[1] += 1
+            else:
+                assert(action == 3)
+                if not self._is_wall((state[0] - 1, state[1])):
+                    state[0] -= 1
         return state
 
-    def is_terminal(self, state=None):
-        if state is None:
-            state = self.get_state()
-        return (state[0] == self.terminal_state[0]) and \
-               (state[1] == self.terminal_state[1])
-
-    def is_wall(self, state):
-        return self.walls[state[0], state[1]]
-
-    def get_reward(self, state=None):
-        if state is None:
-            state = self.get_state()
-        return 1 if self.is_terminal(state) else 0
+    def get_reward(self, old_state, new_state):
+        assert(self._is_valid(old_state))
+        assert(self._is_valid(new_state))
+        return 1 if self._is_terminal(new_state) and not self._is_terminal(old_state) else 0
 
     def reset(self):
         self.set_state(self.initial_state)
         return self.get_state()
 
     def step(self, action):
-        assert(not self.is_terminal())
-        self.set_state(self.get_next_state(action))
-        return self.get_state(), self.get_reward(), self.is_terminal()
+        assert(not self._is_terminal(self.get_state()))
+        old_state = self.get_state()
+        new_state = self.get_next_state(action)
+        self.set_state(new_state)
+        reward = self.get_reward(old_state, new_state)
+        done = self._is_terminal(new_state)
+        return new_state, reward, done
 
-    def get_state_values(self, policy, epsilon):
+    def render(self, show=True):
+        state = self.get_state()
+        sketch = self.walls.astype(np.uint8)
+        sketch[state[0], state[1]] = 2
+        sketch[self.terminal_state[0], self.terminal_state[1]] = 3
+        sketch = self.pretty_print(sketch)
+        if show:
+            print(sketch)
+        return sketch
+
+    def get_state_values(self, policy, epsilon=1e-6):
+        assert(0 < epsilon)
         rv = np.zeros(self.card, dtype=np.float32)
-        delta = float('inf')
+        delta = np.inf
         while delta > epsilon:
             delta = 0
             for x in range(self.card[0]):
                 for y in range(self.card[1]):
                     state = np.array([x, y])
-                    if self.is_wall(state) or self.is_terminal(state):
+                    if self._is_wall(state):
                         continue
-                    old_state_value = rv[x, y]
-                    rv[x, y] = 0
+                    old_value = rv[x, y]
+                    new_value = 0
                     for action in range(self.num_actions):
                         next_state = self.get_next_state(action, state)
-                        reward = self.get_reward(next_state)
+                        reward = self.get_reward(state, next_state)
                         next_state_value = rv[next_state[0], next_state[1]]
-                        rv[x, y] += policy(action, state) * \
-                            (reward + self.gamma * next_state_value)
-                    delta = max(delta, abs(old_state_value - rv[x, y]))
+                        new_value += policy(action, state) * (reward + self.gamma * next_state_value)
+                    rv[x, y] = new_value
+                    delta = max(delta, abs(old_value - new_value))
         return rv
 
-    def get_optimal_values(self, terminal_state=None):
-        if terminal_state is None:
-            terminal_state = self.terminal_state
+    def get_optimal_values(self):
 
         def get_neighbours(state):
             rv = list()
-            if not self.is_wall((state[0], state[1] - 1)):
+            if not self._is_wall((state[0], state[1] - 1)):
                 rv.append((state[0], state[1] - 1))
-            if not self.is_wall((state[0] + 1, state[1])):
+            if not self._is_wall((state[0] + 1, state[1])):
                 rv.append((state[0] + 1, state[1]))
-            if not self.is_wall((state[0], state[1] + 1)):
+            if not self._is_wall((state[0], state[1] + 1)):
                 rv.append((state[0], state[1] + 1))
-            if not self.is_wall((state[0] - 1, state[1])):
+            if not self._is_wall((state[0] - 1, state[1])):
                 rv.append((state[0] - 1, state[1]))
             return rv
 
@@ -117,69 +145,53 @@ class GridWorld:
         queue = list()
         for x in range(self.card[0]):
             for y in range(self.card[1]):
-                if self.is_wall((x, y)):
+                if self._is_wall((x, y)):
                     continue
                 queue.append((x, y))
-        rv[terminal_state[0], terminal_state[1]] = 0
+        rv[self.terminal_state[0], self.terminal_state[1]] = 0
 
         while queue:
             queue.sort(key=lambda v: rv[v[0], v[1]])
             x1, y1 = queue.pop()
-            reward = 1 if x1 == terminal_state[0] and y1 == terminal_state[1] else 0
+            reward = 1 if x1 == self.terminal_state[0] and y1 == self.terminal_state[1] else 0
             neighbours = get_neighbours((x1, y1))
             for x2, y2 in neighbours:
                 if rv[x2, y2] < reward + self.gamma * rv[x1, y1]:
                     rv[x2, y2] = reward + self.gamma * rv[x1, y1]
 
         # clean up
-        rv[terminal_state[0], terminal_state[1]] = 0
+        rv[self.terminal_state[0], self.terminal_state[1]] = 0
         for x in range(self.card[0]):
             for y in range(self.card[1]):
                 state = np.array([x, y])
-                if self.is_wall(state):
+                if self._is_wall(state):
                     rv[x, y] = 0
 
         return rv
 
-    def get_action_values(self, policy, epsilon):
+    def get_action_values(self, policy, epsilon=1e-6):
         state_values = self.get_state_values(policy, epsilon)
         rv = np.zeros(state_values.shape + (self.num_actions,), dtype=np.float32)
         for x in range(self.card[0]):
             for y in range(self.card[1]):
                 state = np.array([x, y])
-                if self.is_wall(state):
+                if self._is_wall(state):
                     continue
                 for action in range(self.num_actions):
                     next_state = self.get_next_state(action, state)
-                    reward = self.get_reward(next_state)
+                    reward = self.get_reward(state, next_state)
                     rv[state[0], state[1], action] = reward + self.gamma * \
                         state_values[next_state[0], next_state[1]]
         return rv
 
-    def get_value_range(self):
-        optimal_values = self.get_optimal_values()
-        for x in range(self.card[0]):
-            for y in range(self.card[1]):
-                if self.is_wall((x, y)) or self.is_terminal((x, y)):
-                    optimal_values[x, y] = np.nan
-        return (np.nanmin(optimal_values), np.nanmax(optimal_values))
-
-    def print_grid(self, grid):
-        to_print = ''
+    def pretty_print(self, grid):
+        rv = ''
         if isinstance(grid[0, 0], np.uint8):
             for row in np.transpose(grid):
-                to_print += ' '.join([str(v) for v in row]) + '\n'
+                rv += ' '.join([str(v) for v in row]) + '\n'
         elif isinstance(grid[0, 0], np.float32):
             for row in np.transpose(grid):
-                to_print += ' '.join(['{0:.2f}'.format(v) for v in row]) + '\n'
+                rv += ' '.join(['{0:.2f}'.format(v) for v in row]) + '\n'
         else:
             raise TypeError('unsupported type {}'.format(type(grid[0, 0])))
-        print(to_print)
-
-    def show(self, state=None):
-        if state is None:
-            state = self.get_state()
-        sketch = self.walls.astype(np.uint8)
-        sketch[state[0], state[1]] = 2
-        sketch[self.terminal_state[0], self.terminal_state[1]] = 3
-        self.print_grid(sketch)
+        return rv

plot.py
@@ -13,27 +13,21 @@ def parse_args():
     parser = argparse.ArgumentParser(
         description='')
     parser.add_argument(
-        '--gamma',
-        type=float,
-        required=True,
-        help='')
-    parser.add_argument(
-        '--outfile',
+        'outfile',
         type=str,
-        required=True,
         help='')
     args = vars(parser.parse_args())
-    assert(0 <= args['gamma'] < 1)
     return args
 
 
-def run(gamma, outfile):
+def run(outfile):
     fig, axarr = plt.subplots(1, 3, figsize=(10, 2), dpi=300)
     sns.set_style('ticks')
     sns.set_context('notebook')
 
-    env = envs.GridWorld(gamma)
-    vmin, vmax = env.get_value_range()
+    env = envs.GridWorld()
+    vmin = env.min_value
+    vmax = env.max_value
     nan_grid = np.ones(env.walls.shape, dtype=np.float32) * np.nan
     walls_grid = np.zeros(env.walls.shape, dtype=np.float32)
     walls_grid[env.walls] = np.nan
@@ -47,7 +41,7 @@ def run(gamma, outfile):
                 mask=np.isnan(grid),
                 center=0.5,
                 cmap=sns.color_palette('coolwarm', np.prod(grid.shape) ** 3, as_cmap=True),
-                cbar_kws={'location': 'left', 'label': 'Value', 'ticks': [0.25, 0.5, 0.75, 1.0]},
+                cbar_kws={'location': 'left', 'label': 'Value', 'ticks': [0.0, 0.5, 1.0]},
                 xticklabels=False,
                 yticklabels=False,
                 linewidth=0,
@@ -80,13 +74,13 @@ def run(gamma, outfile):
 
     # load data
     raw_data = list()
-    for i in range(60):
+    for i in range(200):
         with open('{}.json'.format(i), 'r') as infile:
             raw_data.append(json.load(infile))
-    data = {k1: {k2: list() for k2 in ['RMSVE', 'Initial State Value']}
-            for k1 in ['new', 'policy_iteration']}
+    data = {k1: {k2: list() for k2 in ['RMSVE', 'Return']}
+            for k1 in ['reward_weighted_regression', 'policy_iteration']}
     for entry in raw_data:
-        for k2 in ['RMSVE', 'Initial State Value']:
+        for k2 in ['RMSVE', 'Return']:
             data[entry['args']['mode']][k2].append(entry['record'][k2])
     for k1 in data.keys():
         for k2 in data[k1].keys():
@@ -95,9 +89,10 @@ def run(gamma, outfile):
 
     # plot RMSVE
     k2 = 'RMSVE'
-    for i, k1 in enumerate(['new', 'policy_iteration']):
-        label = {'new': 'New', 'policy_iteration': 'Policy Iteration'}[k1]
-        x = np.arange(26)
+    for i, k1 in enumerate(['reward_weighted_regression', 'policy_iteration']):
+        label = {'reward_weighted_regression': 'RWR',
+                 'policy_iteration': 'PI'}[k1]
+        x = np.arange(41)
         y = np.mean(data[k1][k2], axis=0)
         yerr = np.std(data[k1][k2], axis=0)
         axarr[1].plot(
@@ -113,16 +108,16 @@ def run(gamma, outfile):
             alpha=0.3)
     axarr[1].set_xlabel('Iteration', labelpad=5)
     axarr[1].set_ylabel('RMSVE', labelpad=5)
-    axarr[1].set_xticks([0, 10, 20])
+    axarr[1].set_xticks([0, 20, 40])
     axarr[1].set_yticks([0.0, 0.25, 0.5])
-    axarr[1].set_xlim(0, 25.5)
+    axarr[1].set_xlim(0, 41)
     axarr[1].set_ylim(- 0.05, 0.55)
     axarr[1].legend(loc='best', frameon=False)
 
     # plot initial state value
-    k2 = 'Initial State Value'
-    for i, k1 in enumerate(['new', 'policy_iteration']):
-        x = np.arange(26)
+    k2 = 'Return'
+    for i, k1 in enumerate(['reward_weighted_regression', 'policy_iteration']):
+        x = np.arange(41)
         y = np.mean(data[k1][k2], axis=0)
         yerr = np.std(data[k1][k2], axis=0)
         axarr[2].plot(
@@ -136,11 +131,11 @@ def run(gamma, outfile):
             color=colors[i],
             alpha=0.3)
     axarr[2].set_xlabel('Iteration', labelpad=5)
-    axarr[2].set_ylabel('Initial State Value', labelpad=5)
-    axarr[2].set_xticks([0, 10, 20])
+    axarr[2].set_ylabel('Return', labelpad=5)
+    axarr[2].set_xticks([0, 20, 40])
     axarr[2].set_yticks([0.0, 0.1, 0.2])
-    axarr[2].set_xlim(0, 25.5)
-    axarr[2].set_ylim(- 0.005, 0.225)
+    axarr[2].set_xlim(0, 41)
+    axarr[2].set_ylim(- 0.02, 0.22)
 
     # clean up and save plots
     fig.subplots_adjust(bottom=0.25, wspace=0.4)
@@ -148,7 +143,7 @@ def run(gamma, outfile):
 
 
 def main(args):
-    run(args['gamma'], args['outfile'])
+    run(args['outfile'])
 
 
 if __name__ == '__main__':

run.py
@@ -16,21 +16,24 @@ def parse_args():
         description='')
     parser.add_argument(
         'mode',
-        choices=['policy_iteration', 'new'],
+        choices=['policy_iteration', 'reward_weighted_regression'],
         help='')
     parser.add_argument(
         '--seed',
         type=int,
+        required=False,
         help='')
     parser.add_argument(
         '--gamma',
         type=float,
-        required=True,
+        required=False,
+        default=0.9,
         help='')
     parser.add_argument(
         '--epsilon',
         type=float,
         required=False,
+        default=1e-6,
         help='')
     parser.add_argument(
         '--num-iter',
@@ -58,6 +61,24 @@ def parse_args():
     return args
 
 
+def get_return(env, policy, epsilon):
+    action_set = np.arange(env.num_actions)
+    discount = 1
+    done = False
+    rv = 0
+    state = env.reset()
+    while not done:
+        logits = np.array([policy(action, state) for action in action_set])
+        assert(abs(sum(logits) - 1) < 1e-2)  # ensure precision error propagation is small
+        action = np.random.choice(action_set, p=logits / sum(logits))
+        state, reward, done = env.step(action)
+        rv += discount * reward
+        discount *= env.gamma
+        if discount < epsilon:
+            break
+    return rv
+
+
 def run(env,
         mode,
         gamma,
@@ -69,7 +90,7 @@ def run(env,
     weights = np.random.uniform(size=env.card.tolist() + [env.num_actions])
     for x in range(env.card[0]):
         for y in range(env.card[1]):
-            if env.is_wall((x, y)):
+            if env._is_wall((x, y)):
                 weights[x, y, :].fill(0)
             else:
                 weights[x, y, :] /= np.sum(weights[x, y, :])
@@ -95,14 +116,14 @@ def run(env,
         value_error = list()
         for x in range(env.card[0]):
             for y in range(env.card[1]):
-                if env.is_wall((x, y)) or env.is_terminal((x, y)):
+                if env._is_wall((x, y)):
                     continue
                 value_error.append(optimal_values[x, y] - state_values[x, y])
         entry = {
             'Initial State Absolute Value Error':
                 float(abs(optimal_values[1, 1] - state_values[1, 1])),
-            'Initial State Value':
-                float(state_values[1, 1]),
+            'Return':
+                get_return(env, policy, epsilon),
             'RMSVE':
                 float(np.sqrt(np.mean(np.square(value_error))))
         }
@@ -117,13 +138,14 @@ def run(env,
         # update policy
         for x in range(env.card[0]):
             for y in range(env.card[1]):
-                if env.is_wall((x, y)) or env.is_terminal((x, y)):
+                if env._is_wall((x, y)) or env._is_terminal((x, y)):
                     continue
                 for action in range(env.num_actions):
                     if mode == 'policy_iteration':
                         if action_values[x, y, action] > action_values[x, y, action_map[x, y]]:
                             action_map[x, y] = action
                     else:
+                        assert(mode == 'reward_weighted_regression')
                         weights[x, y, action] *= action_values[x, y, action] / state_values[x, y]
 
         # evaluate new policy